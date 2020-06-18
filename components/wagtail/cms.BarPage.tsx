7706ce27c503df9c83c2bd2fac3da07f22728b3b
David Burke <david@burkesoftware.com>
2020-06-18 10:23:36 -0400

    Links between bar and foo pages

--- components/wagtail/cms.BarPage.tsx
+++ components/wagtail/cms.BarPage.tsx
@@ -1,13 +1,15 @@
-// import fetch from 'node-fetch';
 import { GetServerSideProps } from 'next';
+import Link from 'next/link'
 
 const BarPage = (props: unknown) => {
   return (
     <div>
       hi bar
+      <Link href="/foo" prefetch={false}>Go to foo</Link>
       <pre>
         <code>{JSON.stringify(props, null, 2)}</code>
       </pre>
+      baaaaaaaaaaaaaaaaaaar
     </div>
   );
 };
--- components/wagtail/cms.FooPage.tsx
+++ components/wagtail/cms.FooPage.tsx
@@ -1,13 +1,15 @@
-// import fetch from 'node-fetch';
 import { GetServerSideProps } from 'next';
+import Link from 'next/link'
 
 const FooPage = (props: unknown) => {
   return (
     <div>
       hi Foo
+      <Link href="/bar" prefetch={false}>Go bar</Link>
       <pre>
         <code>{JSON.stringify(props, null, 2)}</code>
       </pre>
+      Foooooooooooooooooooooooooooooooooooooooooo
     </div>
   );
 };
