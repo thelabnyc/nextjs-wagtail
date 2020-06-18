2ff1140e6ee04f9fb388765cdd0fdec3305f81cb
JNaftali <jmarantz@thelabnyc.com>
2020-06-18 14:18:55 -0400

    add comments explaining customization

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -16,6 +16,33 @@ const cmsPageRouter: WagtailRouterConfig = [
 
 const Page = createRouter(cmsPageRouter);
 
+/* If you need to customize page:
+
+const CustomPage = (props: WagtailProps) => {
+  return (
+    <div>
+      <h1>hi</h1>
+      <Page {...props} />
+    </div>
+  )
+}
+export default CustomPage
+
+*/
 export default Page;
 
+/* If you need to customize getServerSideProps
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const wagtailProps = await getCMSProps(context)
+  return {
+    ...wagtailProps,
+    props: {
+      ...wagtailProps.props,
+      myCoolProp: 42,
+    }
+  }
+}
+
+*/
 export const getServerSideProps: GetServerSideProps = getCMSProps;
